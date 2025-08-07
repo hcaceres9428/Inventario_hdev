c89ec499662af164a97d111cca2f397ce05df1dc
Herson Caceres <hcaceres9428>
2025-08-06 22:27:08 -0500

    Add ProductoRepository to handle CRUD operations for Producto entity

--- inventario/src/main/java/com/hdev/inventario/repository/ProductoRepository.java
+++ inventario/src/main/java/com/hdev/inventario/repository/ProductoRepository.java
@@ -1,3 +1,12 @@
-public class ProductoRepository {
+package com.hdev.inventario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hdev.inventario.model.Producto;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
     
 }
