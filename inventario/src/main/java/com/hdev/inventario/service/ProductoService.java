3edb08e84a7d948438a598eab9201461059bc6c3
Herson Caceres <hcaceres9428>
2025-08-06 22:52:19 -0500

    feat: add service for CRUD operations on Producto entity

--- inventario/src/main/java/com/hdev/inventario/service/ProductoService.java
+++ inventario/src/main/java/com/hdev/inventario/service/ProductoService.java
@@ -1,3 +1,34 @@
+package com.hdev.inventario.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hdev.inventario.model.Producto;
+import com.hdev.inventario.repository.ProductoRepository;
+
+@Service
 public class ProductoService {
-    
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> obteneTodos() {
+        return productoRepository.findAll();
+    }
+
+    public Optional<Producto> obtenerPorId(Long id){
+
+        return  productoRepository.findById(id);
+    }    
+
+    public Producto guardar(Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    public void eliminar(Long id){
+        productoRepository.deleteById(id);
+    }
 }
