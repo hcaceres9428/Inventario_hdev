82ca8213a2796caeb32a93dfeae8ea65de02cd0c
Herson Caceres <hcaceres9428>
2025-08-08 22:51:22 -0500

    Fix MySQL connection and annotation in ProductoRepository

--- inventario/src/main/java/com/hdev/inventario/repository/ProductoRepository.java
+++ inventario/src/main/java/com/hdev/inventario/repository/ProductoRepository.java
@@ -1,12 +1,11 @@
 package com.hdev.inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hdev.inventario.model.Producto;
 
-import jakarta.persistence.Entity;
-
-@Entity
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     
 }
